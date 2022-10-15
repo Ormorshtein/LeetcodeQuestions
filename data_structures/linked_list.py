b574b5324425e4697bc6f697d0f3832f5ab1cfb9
or morshtein <ormorshtein@mail.tau.ac.il>
2022-10-15 13:18:44 +0300

    added method that transforms array into linked list

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,4 +1,22 @@
+from typing import List
+
+
 class ListNode:
     def __init__(self, val=0, next_node=None):
         self.val = val
         self.next_node = next_node
+
+
+def array_to_linked_list(arr: List) -> ListNode:
+    """
+    :param arr: list of any type
+    :return: ListNode (=head) if arr is not empty
+            None o.w
+    """
+    sentinel: ListNode = ListNode()
+    prev_node = sentinel
+    for cell in arr:
+        new_node: ListNode = ListNode(val=cell)
+        prev_node.next_node = new_node
+        prev_node = new_node
+    return sentinel.next_node
